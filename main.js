1a289bbe0744da806c518194f45a4538d4aacd89
unknown <lucaagi2005@gmail.com>
2021-03-09 13:48:40 +0100

    initial bot commit for Heroku

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fetch = require('node-fetch');
 const querystring = require('querystring');
 const prefix = "$"
 
+const talkedRecently = new Set();
 const trim = (str, max) => (str.length > max ? `${str.slice(0, max - 3)}...` : str);
 // ##########################################################################
 client.once('ready', () => {
@@ -21,14 +23,29 @@ const command = args.shift().toLowerCase();
 // ###########################################################################
 if (!message.content.startsWith(prefix) || message.author.bot) return;
 
+if (talkedRecently.has(message.author.id))
+  return;
+
+talkedRecently.add(message.author.id);
+setTimeout(() => {
+  talkedRecently.delete(message.author.id);
+}, 1500);
+
+// ############################################################################
+
+const emcol = ["#c28500","#c1d0e4","#009bff","#e9ebee","#e2b659","#e9edf2","#b9b9b9","#017c91","#ff80a8","#8fbc8f","#00b8ff","#3b0099","#0038fa","#2249aa","#ff003c","#ff280a","#a92323","#ff0000","#bbdcf2","#a5d1ee"];
+
+     const emcolor = emcol[Math.floor(Math.random()*emcol.length)]
+
+
    if (command === 'help') {
      const bed = new Discord.MessageEmbed()
-    .setColor('#58f8ff')
+    .setColor(emcolor)
     .setTitle('Help')
     .setAuthor(message.author.username, message.author.displayAvatarURL({ format: "png", dynamic: true }))
     .setDescription(`Prefix is ${prefix}`)
     .addFields(
-		{ name: 'OurGang', value: 'Lists ourgang commands...', inline: true  },
+		{ name: '**OurGang**', value: 'Lists ourgang commands...', inline: true  },
 		{ name: 'Support', value: 'Sends invite to support server', inline: true  },
 		{ name: 'die', value: 'you die for something', inline: true  },
         { name: 'invite', value: 'Sends invite for the bot', inline: true  },
@@ -56,7 +73,8 @@ else if (command === 'die') {
 }
    else if (command === 'zap') {
     const exampleEmbed = new Discord.MessageEmbed()
-	.setColor('#58f8ff')
+    .setAuthor(message.author.username, message.author.displayAvatarURL({ format: "png", dynamic: true }))
+	.setColor(emcolor)
 	.setTitle('Zappatron')
 	.setURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
 	.setDescription('Zappatron, AKA Zap, is a gay boy. He is 13 years old and like brawlstarsp. He is a horni boi, expert csv modder, likes 8-bit, and have a partner, called PedroAPD. They are Shipped and called "Zappedro"')
@@ -65,7 +83,8 @@ else if (command === 'die') {
     }
    else if (command === 'sceptile') {
      const Embed = new Discord.MessageEmbed()
-       .setColor('#58f8ff')
+     .setAuthor(message.author.username, message.author.displayAvatarURL({ format: "png", dynamic: true }))
+       .setColor(emcolor)
        .setTitle('Sceptile380')
        .setDescription('Sceptile380 is a 15 years old boy from Italy. He does lots of dumb jokes about his country, but no one cares. He likes Pokemon since he was 6, and likes Brawl Stars because of Modding. He is also a good Modder and can make custom skins.')
         .setFooter('he is a green boi with an eyepatch that he acutally don\'t need.');
@@ -75,7 +94,8 @@ else if (command === 'die') {
 
     else if (command === 'sand') {
         const Emb = new Discord.MessageEmbed()
-          .setColor('#ff32ec')
+        .setAuthor(message.author.username, message.author.displayAvatarURL({ format: "png", dynamic: true }))
+          .setColor(emcolor)
           .setTitle('Mr.Sandman')
           .setDescription('Mr. Sandman, or just Sand is a 12 years old boi that likes the character Sandy from Brawl Stars. He cured his depression thanks to his Discord friends. He likes to draw and does it very well. He tried to escape Our brawl...but there is no WAY.Now he is a protogen furry.')
            .setFooter('he is secretly sandsunderman');
@@ -85,7 +105,8 @@ else if (command === 'die') {
 
     else if (command === 'andrew') {
                const andrew = new Discord.MessageEmbed()
-             .setColor('#ff32ec')
+               .setAuthor(message.author.username, message.author.displayAvatarURL({ format: "png", dynamic: true }))
+             .setColor(emcolor)
              .setTitle('Andrew')
              .setDescription('Andrew. is a...half Steve from Minecraft amd half Funtime Foxy. He is a notorious Modder and skin maker and is a friend of TheAmazingCC. He is famous for the "Arcade Shelly" skin that he made for ReBrawl')
               .setFooter('but nowdays nobody plays that skin.');
@@ -94,7 +115,8 @@ else if (command === 'die') {
 }
    else if (command === 'delta') {
             const delta = new Discord.MessageEmbed()
-                .setColor('#ff32ec')
+            .setAuthor(message.author.username, message.author.displayAvatarURL({ format: "png", dynamic: true }))
+                .setColor(emcolor)
                 .setTitle('Delta')
                 .setDescription('Retarded as hell. Likes coding and undertale. Also very lazy. Hates ships. Positive')
                 .setFooter('to coronavirus');
@@ -103,25 +125,28 @@ else if (command === 'die') {
 
     else if (command === 'serious') {
                const serious = new Discord.MessageEmbed()
-                    .setColor('#06fff8')
+               .setAuthor(message.author.username, message.author.displayAvatarURL({ format: "png", dynamic: true }))
+                    .setColor(emcolor)
                     .setTitle('Ocean man')
-                    .setDescription('You know who is this guy? That one guy who loves dolphins. He hates UwU. Yes. Sometimes he being frick. just dont say "uwu" to this guy. please.')
-                    .setFooter('simps chinre but he doesnt want to let it be known');
+                    .setDescription('oh, you wanna know about this guy? This guy love dolphins.and yeah... this guy is nothing special.')
+                    .setFooter('SERIOUS LOL');
        message.channel.send(serious)
 }
     else if (command === 'skm') {
                  const skm  = new Discord.MessageEmbed()
-                    .setColor('#06fff8')
+                 .setAuthor(message.author.username, message.author.displayAvatarURL({ format: "png", dynamic: true }))
+                    .setColor(emcolor)
                     .setTitle('SKM')
                     .setDescription('SKM - A human being obsessed with modding Brawl Stars, playing Cookie Run, going to school and secretly saving random pictures of Cirno Fumo on his 1TB hard drive.')
-                    .setFooter('old cum man guy');
+                    .setFooter('lol h');
           message.channel.send(skm)
 
 }
 
-    else if (command === 'eal') {
+else if (command === 'eal') {
                     const eal  = new Discord.MessageEmbed()
-                        .setColor('#06fff8')
+                        .setColor(emcolor)
+                        .setAuthor(message.author.username, message.author.displayAvatarURL({ format: "png", dynamic: true }))
                         .setTitle('Ealanedem')
                         .setDescription('Professional idot. 17y old, known for making Brawl Stars csv mods and hes a moon')
                         .setFooter('do not the cat is gon');
@@ -130,7 +155,8 @@ else if (command === 'die') {
 
     else if (command === 'bea') {
                        const be  = new Discord.MessageEmbed()
-                        .setColor('#ff9919')
+                        .setColor(emcolor)
+                        .setAuthor(message.author.username, message.author.displayAvatarURL({ format: "png", dynamic: true }))
                         .setTitle('Bea')
                         .setDescription('Bea is a beesexual 13 years old. He says "Ok" too many times and wanted to delete Our Brawl for no reasons, but it got deleted anyways. Bee free!')
                         .setFooter('understandable, have a mice day.');
@@ -151,7 +177,8 @@ else if (command === 'die') {
 
     else if (command === 'spunge') {
                            const spunj  = new Discord.MessageEmbed()
-                            .setColor('#ff9919')
+                           .setAuthor(message.author.username, message.author.displayAvatarURL({ format: "png", dynamic: true }))
+                            .setColor(emcolor)
                             .setTitle('Spungebob')
                             .setDescription('Spunge is a 13 yeard old boy that likes to chat on discord and play brawl stars. I try not to go mentaly insane because of school and i can have mental breakdown. Im very weird sometimes, and a big dumbass.')
                             .setFooter('simp');
@@ -227,10 +254,17 @@ client.on("message", async message => {
   }, ms(mutetime));
     }; */
 else if (command === 'cat') {
-      const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
 
-      message.channel.send(file);
-  }
+      const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
+const fembed = new Discord.MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Cat')
+	.setAuthor(message.author.username, message.author.displayAvatarURL({ format: "png", dynamic: true }))
+	.setImage(file)
+	.setTimestamp();
+
+message.channel.send(fembed)
+}
 
 
  });
